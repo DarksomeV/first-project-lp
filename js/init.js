c0f4cad3f632dd8f34b13566f41e1c09a0b29d99
Viktor <viktor.khyshnyak@gmail.com>
2018-04-02 20:48:01 +0300

    Burger

--- js/init.js
+++ js/init.js
@@ -8,4 +8,9 @@
   	prevArrow:'<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
   	nextArrow:'<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
   });
+  //Init Slick nav
+  $('.header-nav').slicknav({
+  	appendTo:'.header .container-fluid',
+  	label:''
+  });
  });
